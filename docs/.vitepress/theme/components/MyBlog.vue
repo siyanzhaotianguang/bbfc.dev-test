1f5ef85866a53694f92f1157cadc432babdaabc6
Lyman Lai <lyman@yaha.me>
2021-01-05 17:28:50 +0800

    ✨  update content

--- docs/.vitepress/theme/components/MyBlog.vue
+++ docs/.vitepress/theme/components/MyBlog.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="page">
     <div class="container">
-      <div class="item" v-for="item in items" :key="item.title">
+      <div class="item" v-for="item in items" :key="item">
         <a :href="item.url" class="feature-img"
           ><img v-if="item.img" :src="item.img"
         /></a>
@@ -25,8 +25,58 @@
 
 <script setup lang="ts">
 import { ref, onMounted, nextTick } from 'vue'
+const limit = 10
+const items = ref([])
+const more = ref(null)
 
 const itemsPoll = [
+  {
+    url: '/blog/21',
+    title: `这两个特性是区块链作为价值互联网的基石`,
+    content:
+      '区块链具有数据不可篡改、可追溯的特性。这个特性使得区块链就像历史一样，发生了就是发生了，无法追悔，野史也无法代替正史写入人类的共识。区块链让数据归属权得到了确权，使得价值可以在互联网中传递。是什么让区块链拥有了这两大杀器呢？首先需要了解区块链的本质。'
+  },
+  {
+    url: '/blog/20',
+    title: '一文读懂区块链相较互联网的真正创新之处',
+    img: '/blog/image/020-00.png',
+    content:
+      '最近关于区块链革命的提法少了很多，我们很少再听到“区块链即将颠覆互联网”的提法，这似乎也寓意着区块链技术（加密技术）正在进入一个理性发展的阶段，如果按照Gartner的技术成熟度曲线来看，区块链正在进入一个泡沫幻灭的阶段，如之前再成熟度曲线中消失的“云计算”概念一样，区块链似乎正在成为一个IT行业的基础设施。数字经济时代，本文试图对区块链（本文中将加密Crypto与区块链blockchain视作等同概念）与互联网的内在关系进行一个梳理。'
+  },
+  {
+    url: '/blog/19',
+    title: '公链之路，破局何方？',
+    img: '/blog/image/019-02.jpg',
+    content:
+      '9月底，比原链（bytom）创始人兼CEO段新星辞去职务，离开了他一直看好的公链舞台。在这半个月之前，另一老牌国产公链公信宝（GXC）的运营主体因涉及非法爬虫业务遭警方查封，一时令舆论哗然。'
+  },
+  {
+    url: '/blog/18',
+    title: '这么多年，为什么99.9%人还没见过区块链是什么样子？',
+    content:
+      '比特币到现在已经十年了，而区块链作为比特币的底层技术被提出也有好几年的时间，发展到现在各种协议和技术不断出现。那么为什么到现在，99.9%的人都没见过区块链？区块链的应用落地场景也并未普及呢？'
+  },
+  {
+    url: '/blog/17',
+    title: '学习| 为什么密码学是区块链的核心',
+    img: '/blog/image/17-01.jpg',
+    content:
+      '大家都知道，区块链是比特币的底层技术，但区块链技术的核心，又是什么？是密码学。密码学是区块链技术的核心。所有的交易信息都会被编码到区块里，而区块链则是由这一个个区块连接在一起而形成的结构。密码技术由来已久，主要经历了古典密码、机械密码、现代密码三个发展阶段。随着历史推进，密码技术不断在演变，密码学成为了科学。而今天我们就要一起来看看，区块链技术当中的密码学，与过去我们了解的密码学有什么不同？它的作用在哪里？'
+  },
+  {
+    url: '/blog/16',
+    title: '比特币不是区块链的全部，创新才是未来',
+    img: '/blog/image/16-01.jpg',
+    content:
+      '一直以来，外界很多人把区块链和比特币混为一谈，认为比特币就是区块链，还形成了一股不小的“共识”。其实，许多人只知道比特币，鲜有听说其它加密货币的存在，一旦听到别人说着陌生的名称，第一个意识自然就是认为这是个“山寨”吧。'
+  },
+  {
+    url: '/blog/15',
+    title: '3个维度, 3种误解, 20大应用场景, 这才是真实的区块链!',
+    img: '/blog/image/15-01.jpg',
+    content:
+      '”区块链+“是相对于”互联网+“而言的，就是“区块链+各个传统行业”，但这并不是简单的两者相加，而是利用区块链技术或特性，让区块链与传统行业深度融合。区块链起源于技术创新，但更重要的是，技术创新带来的模式创新和思维创新。回顾一下互联网+的历程，初始只是将传统企业互联网化、移动化，发展到现在，许多传统企业已经运用“互联网思维”创造了新的商业模式。大体上，拥抱区块链可以分为三个维度：区块链技术、区块链模式和区块链思维。'
+  },
   {
     url: '/blog/14',
     img: '/blog/image/14-01.jpg',
@@ -84,14 +134,11 @@ const itemsPoll = [
   }
 ]
 
-const limit = 10
-const items = ref([])
-const more = ref(null)
-
 function doLoadMore () {
   const top = more.value.offsetTop - 58 * 3
   for (let i = 1; i <= limit; i++) {
-    items.value.push(itemsPoll.splice(0, 1)[0])
+    let item = itemsPoll.splice(0, 1)[0]
+    item && items.value.push(item)
   }
   nextTick(() => {
     window.scrollTo({ top, behavior: 'smooth' })
